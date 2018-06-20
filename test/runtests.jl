f375b87beb39c7b35bfb0cd22c2249b0a9e6388f
Steven G. Johnson <stevenj@mit.edu>
2018-06-20 16:46:10 -0400

    updates for 0.7

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using Hadamard, Compat
-using Base.Test
+using Compat.Test, Compat.LinearAlgebra
 
 H8 = [   1     1     1     1     1     1     1     1
          1     1     1     1    -1    -1    -1    -1
@@ -28,21 +28,23 @@ H8d = [  1     1     1     1     1     1     1     1
          1    -1    -1     1     1    -1    -1     1
          1    -1    -1     1    -1     1     1    -1  ]
 
-ieye(n) = eye(Int8, n)
 
-@test ifwht(eye(8),1) == H8
-@test ifwht(eye(8),2)' == H8
-@test ifwht_natural(eye(8),1) == H8n
-@test ifwht_natural(eye(8),2)' == H8n
-@test ifwht_dyadic(eye(8),1) == H8d
-@test ifwht_dyadic(eye(8),2)' == H8d
+I8 = Matrix{Float64}(I,8,8)
+iI8 = Matrix{Int8}(I,8,8)
 
-@test ifwht(ieye(8),1) == H8
-@test ifwht(ieye(8),2)' == H8
-@test ifwht_natural(ieye(8),1) == H8n
-@test ifwht_natural(ieye(8),2)' == H8n
-@test ifwht_dyadic(ieye(8),1) == H8d
-@test ifwht_dyadic(ieye(8),2)' == H8d
+@test ifwht(I8,1) == H8
+@test ifwht(I8,2)' == H8
+@test ifwht_natural(I8,1) == H8n
+@test ifwht_natural(I8,2)' == H8n
+@test ifwht_dyadic(I8,1) == H8d
+@test ifwht_dyadic(I8,2)' == H8d
+
+@test ifwht(iI8,1) == H8
+@test ifwht(iI8,2)' == H8
+@test ifwht_natural(iI8,1) == H8n
+@test ifwht_natural(iI8,2)' == H8n
+@test ifwht_dyadic(iI8,1) == H8d
+@test ifwht_dyadic(iI8,2)' == H8d
 
 H32 = [  1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
  1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1
@@ -77,14 +79,17 @@ H32 = [  1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
  1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1
  1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 1 -1 ]
 
-@test ifwht(eye(32),1) == H32
-@test ifwht(eye(32),2)' == H32
+ I32 = Matrix{Float64}(I,32,32)
+ iI32 = Matrix{Int8}(I,32,32)
 
-@test ifwht(ieye(32),1) == H32
-@test ifwht(ieye(32),2)' == H32
+@test ifwht(I32,1) == H32
+@test ifwht(I32,2)' == H32
 
-X = @compat reshape(sin.([1:1024*32;]), 1024,32);
-norminf(A) = @compat maximum(abs.(A))
+@test ifwht(iI32,1) == H32
+@test ifwht(iI32,2)' == H32
+
+X = reshape(sin.([1:1024*32;]), 1024,32);
+norminf(A) = maximum(abs.(A))
 
 for f in (:fwht, :fwht_natural, :fwht_dyadic)
     fi = Symbol(string("i", f))
@@ -98,19 +103,19 @@ for f in (:fwht, :fwht_natural, :fwht_dyadic)
 end
 
 @test hadamard(8) == H8n
-@test ifwht_natural(eye(32), 1) == hadamard(32)
-@test ifwht_natural(eye(2), 1) == hadamard(2)
+@test ifwht_natural(I32, 1) == hadamard(32)
+@test ifwht_natural(Matrix{Float64}(I,2,2), 1) == hadamard(2)
 
 print("Checking unitarity of hadamard(n): ")
 for i = 4:4:1000
     H = try
-        convert(Matrix{Int}, hadamard(i))
+        Matrix{Int}(hadamard(i))
     catch
         Int[]
     end
     if !isempty(H)
         print(i, ", ")
-        @test norm(H'*H - size(H,1)*eye(H), 1) == 0
+        @test norm(H'*H - size(H,1)*I, 1) == 0
     end
 end
 println(".\nSuccess!")
